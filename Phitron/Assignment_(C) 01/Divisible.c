eca9f7f6f9acc3c3d45e152add305ef5bc9e4744
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-07-27 01:44:31 +0600

    Add files via upload
    1st

---
+++ Phitron/Assignment_(C) 01/Divisible.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    scanf("%d", &i);
+    if (i%3 == 0)
+    {
+        printf("YES\n", i);
+    }
+    else
+    {
+        printf("NO\n", i);
+    }
+        
+    return 0;
+}
