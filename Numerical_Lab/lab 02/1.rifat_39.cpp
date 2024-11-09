2f6ac0f94a012c386e223aa83062023fd8744b3b
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-11-09 09:07:25 +0600

    Add files via upload

---
+++ Numerical_Lab/lab 02/1.rifat_39.cpp
@@ -0,0 +1,43 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+vector<float> v;
+
+float f(float x) {
+    return 3*x - cos(x) - 1;
+}
+
+float equation(float a, float b) {
+    // for bisection 
+    float c = (a+b)/2;
+
+    if (find(v.begin(), v.end(), c) != v.end())
+        return c;
+    else
+        v.push_back(c);
+
+    if (f(a) * f(c) <= 0)
+        return equation(a, c);
+    else
+        return equation(c, b);
+}
+
+int main()
+{
+    float a, b;
+    cin >> a >> b;
+    while (f(a) * f(b) >= 0) {
+        a++;
+        b++;
+    }
+
+    cout << "The root of the equation (3x - cos(x) - 1) is: ";
+    cout << equation(a, b) << endl;
+
+    return 0;
+}
